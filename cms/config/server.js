4f8b16780cbd61826f8a73b5a6c514d76d824520
Jules <julesgabriel>
2020-08-25 10:00:14 +0200

    back-end finalisé et setup

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3025e755082a3d54c3874780b754f6b2'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '80783005-e363-4193-a2a7-a5f7c51b9ea4'
+};
