546944446e68203cf1811f2f7dfbd79dd8c81237
Jules <julesgabriel>
2020-08-27 20:03:50 +0200

    commit?

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -4,17 +4,15 @@ import CountUp from 'react-countup';
 import Layout from "../components/layout"
 import './style/index.css';
 import SEO from "../components/seo"
-<<<<<<< HEAD
+
 import {graphql, useStaticQuery} from "gatsby";
 
 import Cta from '../components/cta';
 
-const IndexPage = ({data}) => (
-=======
 
 
 const IndexPage = () => (
->>>>>>> 57af438063de5fe91ec1d2923a1f90269f541db5
+
     <Layout>
         <SEO title="Accueil"/>
         <section className="titleSection">
@@ -33,7 +31,7 @@ const IndexPage = () => (
         </section>
         <section className="sectionPresentation">
             <p className="boldText">Le pôle Léonard de Vinci, est composé de 3 écoles (ESILV, EMLV et IIM), regroupant
-<<<<<<< HEAD
+
                 plus de <span>7 000 étudiants</span> ! Comme ça fait un bon paquet de monde, et que c’est cool de
                 rencontrer des gens
                 quand même,
@@ -44,7 +42,7 @@ const IndexPage = () => (
                 pas moins de <span>55 assos</span> ! Il y
                 a <span>du sport</span>, <span>du gaming</span>, <span>de l’événementiel</span>, <span>du théâtre</span>, <span>de la cuisine</span>, <span>de
                 l’aquaponey</span>. Bref t’as vraiment moyen de trouver ton bonheur! <br/><br/>
-=======
+
                 plus de 7 000
                 étudiants ! Comme ça fait un bon paquet de monde, et que c’est cool de rencontrer des gens quand même,
                 la transversalité entre les écoles est hyper présente : semaines transverses, sport obligatoire, et on y
@@ -53,16 +51,18 @@ const IndexPage = () => (
                 pas moins de 55 assos ! Il y a du sport, du gaming, de l’événementiel, du théâtre, de la cuisine, de
                 l’aquaponey. Bref t’as vraiment moyen de trouver ton bonheur! <br/><br/>
 
->>>>>>> 57af438063de5fe91ec1d2923a1f90269f541db5
+
                 Tu vas donc pouvoir te réunir avec des gens qui ont les mêmes centres d'intérêt que toi, vous allez
                 organiser des événements de plus ou moins grande envergure (voyages / compétition / rencontres à
                 thèmatique). Et puis si t’es actif t’as des points bonus pour compenser tes cours mais ça faut pas
                 dire... <br/><br/>
-<<<<<<< HEAD
-                Et donc pour faire tourner toutes ces assos, on a besoin de <span>TOI</span> ! Rejoins-nous dans cette aventure de
+
+                Et donc pour faire tourner toutes ces assos, on a besoin de <span>TOI</span> ! Rejoins-nous dans cette
+                aventure de
                 malade, tu vas apprendre à gérer une équipe, démarcher des entreprises, organiser des événements, et
                 évidemment découvrir des personnes incroyables qui vont devenir ta famille. <br/><br/>
-                On espère que les assos n’ont plus de secrets pour toi, et que t’es <span>motivé.e</span> pour faire vibrer le pôle à
+                On espère que les assos n’ont plus de secrets pour toi, et que t’es <span>motivé.e</span> pour faire
+                vibrer le pôle à
                 nos côtés !
             </p>
         </section>
@@ -82,8 +82,7 @@ const IndexPage = () => (
                 Des membres des associations sont là pour répondre à vos questions <span>toute la journée</span>!
             </p>
             <Cta link="https://discord.gg/X77ztWs" className="buttonDiscord" content="discord.com/X77ztWs"/>
-=======
-
+            <p>
                 Et donc pour faire tourner toutes ces assos, on a besoin de TOI ! Rejoins-nous dans cette aventure de
                 malade, tu vas apprendre à gérer une équipe, démarcher des entreprises, organiser des événements, et
                 évidemment découvrir des personnes incroyables qui vont devenir ta famille. <br/><br/>
@@ -91,12 +90,11 @@ const IndexPage = () => (
                 On espère que les assos n’ont plus de secrets pour toi, et que t’es motivé.e pour faire vibrer le pôle à
                 nos côtés !
             </p>
->>>>>>> 57af438063de5fe91ec1d2923a1f90269f541db5
         </section>
     </Layout>
 )
 
-<<<<<<< HEAD
+
 export const pageQuery = graphql`
 query homeData {
   allStrapiCommons {
@@ -112,6 +110,5 @@ query homeData {
 export default IndexPage
 
 
-=======
 export default IndexPage
->>>>>>> 57af438063de5fe91ec1d2923a1f90269f541db5
+
